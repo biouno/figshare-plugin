eb24870093541e4814634a59a445ac8dcd1c85f3
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-09-05 01:46:34 +1200

    Preparing to include complte integration

--- src/main/java/org/biouno/figshare/FigShareNotifier.java
+++ src/main/java/org/biouno/figshare/FigShareNotifier.java
@@ -24,12 +24,26 @@
 package org.biouno.figshare;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import org.biouno.figshare.credentials.FigShareOauthCredentials;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.domains.DomainRequirement;
+
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
+import hudson.security.ACL;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
+import jenkins.model.Jenkins;
 
 /**
  * Notifier to send artifact to figshare, such as pictures, graphs and other
@@ -41,6 +55,89 @@ import hudson.tasks.Notifier;
 @SuppressWarnings("unchecked")
 public class FigShareNotifier extends Notifier {
 
+	private static final Logger LOGGER = Logger.getLogger(FigShareNotifier.class.getName());
+
+	/**
+	 * Credentials ID;
+	 */
+	private final String credentialsId;
+	/**
+	 * figshare Article title that will be created.
+	 */
+	private final String articleTitle;
+	/**
+	 * figshare Article description.
+	 */
+	private final String articleDescription;
+	/**
+	 * Ant pattern to locate files.
+	 */
+	private final String antPattern;
+
+	/**
+	 * figshare API.
+	 */
+	private final FigShareClient figshare;
+
+	@DataBoundConstructor
+	public FigShareNotifier(String credentialsId, String articleTitle, String articleDescription, String antPattern) {
+		this.credentialsId = credentialsId;
+		this.articleTitle = articleTitle;
+		this.articleDescription = articleDescription;
+		this.antPattern = antPattern;
+
+		// Get credential defined by user, using credential ID
+		List<FigShareOauthCredentials> credentials = CredentialsProvider.lookupCredentials(
+				FigShareOauthCredentials.class, Jenkins.getInstance(), ACL.SYSTEM,
+				Collections.<DomainRequirement> emptyList());
+		FigShareOauthCredentials credential = CredentialsMatchers.firstOrNull(credentials,
+				CredentialsMatchers.allOf(CredentialsMatchers.withId(credentialsId)));
+
+		if (null != credential) {
+			// TBD: externalise this as an advanced option in the job
+			// configuration
+			if (LOGGER.isLoggable(Level.FINE)) {
+				LOGGER.log(Level.FINE, "Initialising the figshare API");
+			}
+			figshare = FigShareClient.to("http://api.figshare.com/", 1, credential.getClientKey(),
+					credential.getClientSecret().getPlainText(), credential.getTokenKey(),
+					credential.getTokenSecret().getPlainText());
+		} else {
+			LOGGER.warning(String.format(
+					"Could not locate credential with ID %s. figshare integration is disabled for this notifier",
+					credentialsId));
+			figshare = null;
+		}
+	}
+
+	/**
+	 * @return the credentialsId
+	 */
+	public String getCredentialsId() {
+		return credentialsId;
+	}
+
+	/**
+	 * @return the articleTitle
+	 */
+	public String getArticleTitle() {
+		return articleTitle;
+	}
+
+	/**
+	 * @return the articleDescription
+	 */
+	public String getArticleDescription() {
+		return articleDescription;
+	}
+
+	/**
+	 * @return the antPattern
+	 */
+	public String getAntPattern() {
+		return antPattern;
+	}
+
 	@Override
 	public BuildStepMonitor getRequiredMonitorService() {
 		return BuildStepMonitor.NONE;
@@ -49,8 +146,19 @@ public class FigShareNotifier extends Notifier {
 	@Override
 	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener)
 			throws InterruptedException, IOException {
-		// TODO Auto-generated method stub
-		return super.perform(build, launcher, listener);
+		listener.getLogger().println("Looking for files to upload to figshare...");
+		FilePath workspace = build.getWorkspace();
+		if (null != workspace) {
+			FilePath[] files = workspace.list(antPattern);
+			if (null != files && files.length > 0) {
+				for (FilePath file : files) {
+					
+				}
+			} else {
+				listener.getLogger().println("No files found. Skip creating an empty figshare article");
+			}
+		}
+		return false;
 	}
 
 }
